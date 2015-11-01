0a55fea87c6a3b91015c0d7d04a18a629390e888
PawelAksiutin <pawel.aksiutin@gmail.com>
2015-11-02 00:14:59 +1100

    Minor improvements

--- src/main/java/com/date/calculator/Main.java
+++ src/main/java/com/date/calculator/Main.java
@@ -9,7 +9,7 @@ public class Main {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static void main(String[] args) {
-        System.out.println("Valid date format is DD/MM/YYYY e.g. 07/11/1972 or 1/1/1972");
+        System.out.println("Valid date format is DD/MM/YYYY e.g. 01/01/1972 or 1/1/1972");
         try {
             System.out.println("Please enter first date.");
             LocalDate date1 = DateFromConsoleReader.readWithRetry(2);
@@ -18,7 +18,7 @@ public class Main {
             LocalDate date2 = DateFromConsoleReader.readWithRetry(2);
 
             int numberOfDays = DaysCalculator.fullDaysBetween(date1, date2);
-            System.out.println(String.format("%d day(s) between %s and %s .", numberOfDays, date1.format(FORMATTER), date2.format(FORMATTER)));
+            System.out.println(String.format("%d day(s) between %s and %s", numberOfDays, date1, date2));
         } catch (DateTimeParseException e) {
             System.err.println("Application will exit.");
         }
