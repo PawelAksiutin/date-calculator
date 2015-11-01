b0ddf930faec0b732f43430073712ad665b50ecd
PawelAksiutin <pawel.aksiutin@gmail.com>
2015-11-01 16:02:35 +1100

    Extracted DateFromConsoleReader

--- src/com/date/calculator/Main.java
+++ src/com/date/calculator/Main.java
@@ -3,11 +3,10 @@ package com.date.calculator;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
 
 public class Main {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d/M/yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static void main(String[] args) {
 
@@ -17,36 +16,16 @@ public class Main {
         LocalDate date2 = null;
         try {
             System.out.println("Please enter fist date.");
-            date1 = readDateFromConsoleWithRetry();
+            date1 = DateFromConsoleReader.readWithRetry(3);
 
             System.out.println("Please enter second date.");
-            date2 = readDateFromConsoleWithRetry();
+            date2 = DateFromConsoleReader.readWithRetry(3);
         } catch (DateTimeParseException e) {
             System.err.println("Application will exit.");
             return;
         }
         int numberOfDays = DaysCalculator.fullDaysBetween(date1, date2);
-        System.out.println(numberOfDays + " days between " + date1.format(FORMATTER) + " and " + date2.format(FORMATTER) + " .");
+        System.out.println(numberOfDays + " day(s) between " + date1.format(FORMATTER) + " and " + date2.format(FORMATTER) + " .");
     }
 
-    private static LocalDate readDateFromConsole() {
-        //todo to extract separate class
-        Scanner scanner = new Scanner(System.in);
-        String stringDate1 = scanner.nextLine();
-        return LocalDate.parse(stringDate1, FORMATTER);
-    }
-
-    private static LocalDate readDateFromConsoleWithRetry() {
-        int numTries = 3;
-        while(true) {
-            try {
-                return readDateFromConsole();
-            } catch (DateTimeParseException e) {
-                if (--numTries < 1) {
-                    throw e;
-                }
-                System.err.println("Date entered is incorrect. Please try again. You have " + numTries + " more attempt(s).");
-            }
-        }
-    }
 }
